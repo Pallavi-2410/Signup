15b24d8f188cf1d03eec8a49a50b6c875a571e8c
Pallavi-2410 <patel.pallavi1819@gmail.com>
2024-12-04 22:18:45 +0530

    added Login

--- index.js
+++ index.js
@@ -9,3 +9,24 @@ function signUp(userName){
 }
 console.log(signUp("Chandan"));
 console.log(signUp("Suyog"));
+
+
+function login(userName, password){
+    let arr = ["Suyog", "Pallavi", "Vibha"];
+    let correctpassword = "Emp@123";
+
+    if(arr.includes(userName)){
+        if(password === correctpassword){
+            return "Login Successful";
+        }else{
+            return "Wrong Password" ;
+        }
+    }else{
+        return "User Not Found, Please Signup";
+    }
+}
+console.log(login("Suyog", "Emp@123"));
+console.log(login("Suyog", "ABC123"));
+console.log(login("Chandan", "ABC123"));
+
+
